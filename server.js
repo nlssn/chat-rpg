3e4c9c1bd6be8ce67c09f027bc6e14c47ba22bb1
Johannes Nilsson <johannes@nilsson.im>
2020-05-20 13:26:23 +0200

    Add Socket.io

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+const io = socketio(server);
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// What happens when a player is connected
+io.on('connection', socket => {
+   console.log('A new connection was established');
+});
 
 // Start listening on specified PORT
 const PORT = 3000 || process.env.PORT;
